0af9017fc98691827c5ce55d0cf3d00ee260ad19
caodongzhen <caodongzhen@ybm100.com>
2020-01-06 14:55:23 +0800

    add redislock 设置锁的过期时间

---
+++ demo/src/main/java/com/example/redislock/LockCase2.java
@@ -0,0 +1,28 @@
+package com.example.redislock;
+
+import redis.clients.jedis.Jedis;
+
+import static com.example.redislock.LockConstants.*;
+
+public class LockCase2 extends RedisLock {
+
+    public LockCase2(Jedis jedis, String name) {
+        super(jedis, name);
+    }
+
+    @Override
+    public void lock() {
+        while(true){
+            String result = jedis.set(lockKey, "value", NOT_EXIST,SECONDS,30);
+            if(OK.equals(result)){
+                System.out.println(Thread.currentThread().getId()+"加锁成功!");
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void unlock() {
+        jedis.del(lockKey);
+    }
+}
